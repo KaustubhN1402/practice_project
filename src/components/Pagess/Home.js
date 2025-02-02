22d053a3d64f541c759c6889327f971406cd8224
Kaustubh Netke <netkekaustubh14@gmail.com>
2025-02-02 11:40:54 +0530

    home page check

--- src/components/Pagess/Home.js
+++ src/components/Pagess/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="text-center py-10">
       
-      <p className="mt-4 text-lg">Homepage</p>
+      <p className="mt-4 text-lg">Homepage completed</p>
     </div>
   );
 };
